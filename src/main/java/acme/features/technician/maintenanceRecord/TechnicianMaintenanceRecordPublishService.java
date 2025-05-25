c15a91d3cea71b2f55fd9c3acb96f24b20bacefc
Nuno José del Pino Escalante <nundelesc@alum.us.es>
2025-05-25 17:35:06 +0200

    Tests and some fixes

--- src/main/java/acme/features/technician/maintenanceRecord/TechnicianMaintenanceRecordPublishService.java
+++ src/main/java/acme/features/technician/maintenanceRecord/TechnicianMaintenanceRecordPublishService.java
@@ -82,22 +82,10 @@ public class TechnicianMaintenanceRecordPublishService extends AbstractGuiServic
 		int id;
 		id = super.getRequest().getData("id", int.class);
 
-		if (!this.getBuffer().getErrors().hasErrors("status"))
-			super.state(maintenanceRecord.getStatus() != null, "status", "acme.validation.technician.maintenance-record.noStatus.message");
-
-		if (!this.getBuffer().getErrors().hasErrors("nextInspectionDate") && maintenanceRecord.getNextInspectionDate() != null)
+		if (!this.getBuffer().getErrors().hasErrors("nextInspectionDate"))
 			super.state(maintenanceRecord.getNextInspectionDate().compareTo(maintenanceRecord.getMoment()) > 0, "nextInspectionDate", "acme.validation.technician.maintenance-record.nextInspectionDate.message");
 
-		if (!this.getBuffer().getErrors().hasErrors("estimatedCost") && maintenanceRecord.getEstimatedCost() != null)
-			super.state(0.00 <= maintenanceRecord.getEstimatedCost().getAmount() && maintenanceRecord.getEstimatedCost().getAmount() <= 1000000.00, "estimatedCost", "acme.validation.technician.maintenance-record.estimatedCost.message");
-
-		if (!this.getBuffer().getErrors().hasErrors("notes") && maintenanceRecord.getNotes() != null)
-			super.state(maintenanceRecord.getNotes().length() <= 255, "notes", "acme.validation.technician.maintenance-record.notes");
-
-		if (!this.getBuffer().getErrors().hasErrors("aircraft") && maintenanceRecord.getAircraft() != null)
-			super.state(this.repository.findAllAircrafts().contains(maintenanceRecord.getAircraft()), "aircraft", "acme.validation.technician.maintenance-record.aircraft.message");
-
-		if (!this.getBuffer().getErrors().hasErrors("published") && maintenanceRecord.getPublished() != null)
+		if (!this.getBuffer().getErrors().hasErrors("published"))
 			super.state(this.repository.findNotPublishedTaskOfMaintenanceRecord(id) == 0 && this.repository.countAllRelatedTaskWithMaintenanceRecord(id) != 0, "*", "acme.validation.technician.maintenance-record.published.message");
 	}
 
@@ -118,7 +106,7 @@ public class TechnicianMaintenanceRecordPublishService extends AbstractGuiServic
 		choices = SelectChoices.from(MaintenanceRecordStatus.class, maintenanceRecord.getStatus());
 		aircraft = SelectChoices.from(aircrafts, "id", maintenanceRecord.getAircraft());
 
-		dataset = super.unbindObject(maintenanceRecord, "status", "nextInspectionDate", "estimatedCost", "notes", "aircraft", "published");
+		dataset = super.unbindObject(maintenanceRecord, "moment", "status", "nextInspectionDate", "estimatedCost", "notes", "aircraft", "published");
 
 		dataset.put("status", choices.getSelected().getKey());
 		dataset.put("status", choices);
