567cee1518969c9fa04f0f0156804ecddf18fac2
alereyper <alereyper@alum.us.es>
2025-05-26 17:54:20 +0200

    Index added

--- src/main/java/acme/entities/trackingLogs/TrackingLog.java
+++ src/main/java/acme/entities/trackingLogs/TrackingLog.java
@@ -4,7 +4,9 @@ package acme.entities.trackingLogs;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Index;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.Valid;
 
 import acme.client.components.basis.AbstractEntity;
@@ -21,6 +23,11 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
+@Table(indexes = {
+	@Index(columnList = "publish, lastUpdateMoment, resolutionPercentage"), @Index(columnList = "resolutionPercentage, publish"), @Index(columnList = "publish"), @Index(columnList = "publish, resolutionPercentage"),
+	@Index(columnList = "publish, resolutionPercentage")
+
+})
 public class TrackingLog extends AbstractEntity {
 
 	// Serialisation version --------------------------------------------------
